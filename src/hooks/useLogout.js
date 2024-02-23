4a1354f1e29f980a7a508ccce6b8e0376e9f7e69
amin-dev-tech <amin.dev.tech@gmail.com>
2024-02-23 20:39:27 +0300

    refactor code

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -7,39 +7,39 @@ import { useLogoutMutation } from "../slices/usersApiSlice";
 import { useSelector, useDispatch } from "react-redux";
 
 const useLogout = () => {
-  const dispatch = useDispatch();
-  const [logoutApiCall] = useLogoutMutation();
-  const { token, refreshToken, expDate } = useSelector((state) => state.auth);
-  const logoutHandler = async () => {
-    try {
-      const res = await logoutApiCall({
-        data: {
-          token: "<string>",
-          refreshToken,
-          error: "<string>",
-          expDate,
-        },
-        token: token,
-      });
-      console.log(res);
-      dispatch(logout());
-      toast.success(res.data.message, {
-        autoClose: 2000,
-        style: {
-          fontSize: "18px",
-        },
-      });
-    } catch (err) {
-      toast.error(err?.data?.message || err.error, {
-        autoClose: 2000,
-        style: {
-          fontSize: "18px",
-        },
-      });
-    }
-  };
+	const dispatch = useDispatch();
+	const [logoutApiCall] = useLogoutMutation();
+	const { token, refreshToken, expDate } = useSelector((state) => state.auth);
+	const logoutHandler = async () => {
+		try {
+			const res = await logoutApiCall({
+				data: {
+					token: "<string>",
+					refreshToken,
+					error: "<string>",
+					expDate,
+				},
+				token: token,
+			});
+			console.log(res);
+			dispatch(logout());
+			toast.success(res.data.message, {
+				autoClose: 2000,
+				style: {
+					fontSize: "18px",
+				},
+			});
+		} catch (err) {
+			toast.error(err?.data?.message || err.error, {
+				autoClose: 2000,
+				style: {
+					fontSize: "18px",
+				},
+			});
+		}
+	};
 
-  return logoutHandler;
+	return logoutHandler;
 };
 
 export default useLogout;
--- src/slices/usersApiSlice.js
+++ src/slices/usersApiSlice.js
@@ -5,39 +5,39 @@ import { USERS_URL_HTTPS } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    login: builder.mutation({
-      query: (data) => ({
-        url: `${USERS_URL_HTTPS}/Login`,
-        method: "POST",
-        body: data,
-        headers: { "Content-Type": "application/json" },
-      }),
-    }),
-    refresh: builder.mutation({
-      query: (data) => ({
-        url: `${USERS_URL_HTTPS}/RefresToken`,
-        method: "POST",
-        body: data,
-        headers: { "Content-Type": "application/json" },
-      }),
-    }),
-    logout: builder.mutation({
-      query: ({ data, token }) => {
-        console.log(data, token);
-        return {
-          url: `${USERS_URL_HTTPS}/Logout`,
-          method: "POST",
-          body: data,
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
-          },
-        };
-      },
-    }),
-  }),
+	endpoints: (builder) => ({
+		login: builder.mutation({
+			query: (data) => ({
+				url: `${USERS_URL_HTTPS}/Login`,
+				method: "POST",
+				body: data,
+				headers: { "Content-Type": "application/json" },
+			}),
+		}),
+		refresh: builder.mutation({
+			query: (data) => ({
+				url: `${USERS_URL_HTTPS}/RefresToken`,
+				method: "POST",
+				body: data,
+				headers: { "Content-Type": "application/json" },
+			}),
+		}),
+		logout: builder.mutation({
+			query: ({ data, token }) => {
+				/*  console.log(data, token); */
+				return {
+					url: `${USERS_URL_HTTPS}/Logout`,
+					method: "POST",
+					body: data,
+					headers: {
+						"Content-Type": "application/json",
+						"Authorization": `Bearer ${token}`,
+					},
+				};
+			},
+		}),
+	}),
 });
 
 export const { useLoginMutation, useRefreshMutation, useLogoutMutation } =
-  usersApiSlice;
+	usersApiSlice;
