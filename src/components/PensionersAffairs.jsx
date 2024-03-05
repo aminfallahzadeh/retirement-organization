e228075284fcbd34b01c22d051b0885d2f10ce19
amin-dev-tech <amin.dev.tech@gmail.com>
2024-03-05 15:48:25 +0300

    add personel form

--- src/components/PensionersAffairs.jsx
+++ src/components/PensionersAffairs.jsx
@@ -1,14 +1,20 @@
 // component imports
 import BankinfoForm from "../components/BankinfoForm";
+import PersonelinfoForm from "./PersonelinfoForm";
 
 // react imports
 import { useState } from "react";
 
 function PensionersAffairs() {
   const [showBankForm, setShowBankForm] = useState(false);
+  const [showPersonelForm, setShowPersonelForm] = useState(false);
 
   const handleShowBankForm = () => {
     setShowBankForm(!showBankForm);
+  };
+
+  const handleShowPersonelForm = () => {
+    setShowPersonelForm(!showPersonelForm);
   };
 
   return (
@@ -17,9 +23,11 @@ function PensionersAffairs() {
         <h4>اطلاعات فردی بازنشسته</h4>
       </div>
 
-      <div className="pensionersAffairs__item">
+      <div className="pensionersAffairs__item" onClick={handleShowPersonelForm}>
         <h4>اطلاعات پرسنلی</h4>
       </div>
+
+      {showPersonelForm && <PersonelinfoForm />}
 
       <div className="pensionersAffairs__item" onClick={handleShowBankForm}>
         <h4>اطلاعات شماره حساب بانکی بازنشسته</h4>
---
+++ src/components/PersonelinfoForm.jsx
@@ -0,0 +1,159 @@
+function PersonelinfoForm() {
+  return (
+    <form method="POST" className="PersonelInfoForm">
+      <div className="inputBox">
+        <input
+          type="text"
+          id="personalCode"
+          className="input field input--dark"
+          required
+        />
+        <label htmlFor="personalCode" className="label label--light">
+          کد ملی (<span>اجباری</span>)
+        </label>
+      </div>
+
+      <div className="inputBox">
+        <input
+          type="text"
+          id="group"
+          className="input field input--dark"
+          required
+        />
+        <label htmlFor="group" className="label label--light">
+          گروه (<span>اجباری</span>)
+        </label>
+      </div>
+
+      <div className="inputBox">
+        <input
+          type="text"
+          id="military"
+          className="input field input--dark"
+          required
+        />
+        <label htmlFor="military" className="label label--light">
+          آخرین محل خدمت (<span>اجباری</span>)
+        </label>
+      </div>
+
+      <div className="inputBox">
+        <input
+          type="text"
+          id="position"
+          className="input field input--dark"
+          required
+        />
+        <label htmlFor="position" className="label label--light">
+          سمت (<span>اجباری</span>)
+        </label>
+      </div>
+      <div className="inputBox">
+        <input
+          type="text"
+          id="type"
+          className="input field input--dark"
+          required
+        />
+        <label htmlFor="type" className="label label--light">
+          نوع استخدام (<span>اجباری</span>)
+        </label>
+      </div>
+
+      <div className="inputBox">
+        <input
+          type="text"
+          id="docs"
+          className="input field input--dark"
+          required
+        />
+        <label htmlFor="docs" className="label label--light">
+          مستند بازنشستگی (<span>اجباری</span>)
+        </label>
+      </div>
+
+      <div className="inputBox">
+        <input
+          type="text"
+          id="retireDate"
+          className="input field input--dark"
+          required
+        />
+        <label htmlFor="retireDate" className="label label--light">
+          تاریخ بازنشستگی (<span>اجباری</span>)
+        </label>
+      </div>
+
+      <div className="inputBox">
+        <input
+          type="text"
+          id="deathDate"
+          className="input field input--dark"
+          required
+        />
+        <label htmlFor="deathDate" className="label label--light">
+          تاریخ فوت
+        </label>
+      </div>
+
+      <div className="PersonelInfoForm__flex">
+        <div className="inputBox PersonelInfoForm__flex--item">
+          <input
+            type="text"
+            id="condition"
+            className="input field input--dark"
+            required
+          />
+          <label htmlFor="condition" className="label label--light">
+            وضعیت
+          </label>
+        </div>
+
+        <div className="inputBox PersonelInfoForm__flex--item">
+          <input
+            type="text"
+            id="manager"
+            className="input field input--dark"
+            required
+          />
+          <label htmlFor="manager" className="label label--light">
+            ضریب مدیریتی
+          </label>
+        </div>
+      </div>
+
+      <div className="inputBox">
+        <input
+          type="text"
+          id="marrage"
+          className="input field input--dark"
+          required
+        />
+        <label htmlFor="marrage" className="label label--light">
+          وضعیت تاهل (<span>اجباری</span>)
+        </label>
+      </div>
+
+      <div className="PersonelInfoForm__box">
+        <div className="PersonelInfoForm__box--item">
+          <input type="checkbox" id="marriageRight" />
+          <label
+            htmlFor="marriageRight"
+            className="PersonelInfoForm__box--label"
+          >
+            حق تاهل
+          </label>
+        </div>
+
+        <div className="PersonelInfoForm__box--item">
+          <input type="checkbox" id="childRight" />
+          <label htmlFor="childRight" className="PersonelInfoForm__box--label">
+            حق اولاد
+          </label>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default PersonelinfoForm;
