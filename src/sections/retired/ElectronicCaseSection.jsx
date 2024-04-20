30afab05e0e4342df693d1de5a0b895a657cc636
amin-dev-tech <amin.dev.tech@gmail.com>
2024-04-20 18:19:26 +0300

    add image preview

--- src/sections/retired/ElectronicCaseSection.jsx
+++ src/sections/retired/ElectronicCaseSection.jsx
@@ -7,34 +7,46 @@ import ArchiveTree from "../../components/ArchiveTree";
 // redux imports
 import { useSelector } from "react-redux";
 
-// library imports
-import imageType from "image-type";
-
 function ElectronicCaseSection() {
   const [previewImage, setPreviewImage] = useState(null);
   const { selectedImageData } = useSelector((state) => state.archiveData);
 
   useEffect(() => {
     if (selectedImageData?.attachment) {
-      // Convert base64 string to Uint8Array
+      // Convert base64 string to ArrayBuffer
       const base64String = selectedImageData.attachment;
-      const bytes = new Uint8Array(
-        base64String.match(/[\s\S]/g).map((ch) => ch.charCodeAt(0))
-      );
-
-      // Detect the image type
-      const detectedType = imageType(bytes);
-
-      if (detectedType) {
-        // If the image type is detected, set the appropriate MIME type and prefix
-        const mimeType = `image/${detectedType.ext}`;
-        const prefix = `data:${mimeType};base64,`;
-
-        // Set the base64 image data as the source of the image element
-        setPreviewImage(`${prefix}${selectedImageData.attachment}`);
-      } else {
-        console.error("Unable to detect image type.");
+      const binaryString = window.atob(base64String);
+      const bytes = new Uint8Array(binaryString.length);
+      for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
       }
+
+      // Detect the image type based on its signature
+      const signature = bytes[0] * 256 + bytes[1];
+      let mimeType;
+      switch (signature) {
+        case 0xffd8: // JPEG
+          mimeType = "image/jpeg";
+          break;
+        case 0x8950: // PNG
+          mimeType = "image/png";
+          break;
+        case 0x4749: // GIF
+          mimeType = "image/gif";
+          break;
+        case 0x424d: // BMP
+          mimeType = "image/bmp";
+          break;
+        default:
+          console.error("Unsupported image type.");
+          return;
+      }
+
+      // Set the appropriate MIME type and prefix
+      const prefix = `data:${mimeType};base64,`;
+
+      // Set the base64 image data as the source of the image element
+      setPreviewImage(`${prefix}${selectedImageData.attachment}`);
     }
   }, [selectedImageData]);
 
