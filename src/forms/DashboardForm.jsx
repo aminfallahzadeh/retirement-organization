f63338a3d289732736caf171e8b30f1ca37e5f52
amin-dev-tech <amin.dev.tech@gmail.com>
2024-08-20 23:02:59 +0300

    add loading modal in dashboard

--- src/forms/DashboardForm.jsx
+++ src/forms/DashboardForm.jsx
@@ -5,6 +5,7 @@ import { useState, useRef } from "react";
 import { useLazyDashboardReportQuery } from "../slices/reportApiSlice";
 
 // mui imports
+import { Box, CircularProgress } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import {
   CalendarTodayOutlined as CalenderIcon,
@@ -23,6 +24,7 @@ import Select from "react-select";
 import makeAnimated from "react-select/animated";
 
 // components
+import Modal from "../components/Modal";
 import DashboardSumGrid from "../grids/DashboardGrids/DashboardSumGrid";
 import DashboardHouseRightGrid from "../grids/DashboardGrids/DashboardHouseRightGrid";
 import UnderWarantyAmountMenGrid from "../grids/DashboardGrids/UnderWarantyAmountMenGrid";
@@ -315,163 +317,185 @@ function DashboardForm() {
   );
 
   const content = (
-    <section className="flex-col u-margin-bottom-md">
-      <form method="POST" className="grid grid--col-5" noValidate>
-        <div className="inputBox__form">
-          <Select
-            closeMenuOnSelect={true}
-            options={retiredTypeOptions}
-            components={animatedComponents}
-            onChange={handleSelectOptionChange}
-            value={retiredTypeOptions.find(
-              (item) => item.value === data?.applicantTypeIsRetired
-            )}
-            name="applicantTypeIsRetired"
-            isClearable={true}
-            placeholder={
-              <div className="react-select-placeholder">نوع بازنشسته</div>
-            }
-            noOptionsMessage={selectSettings.noOptionsMessage}
-            loadingMessage={selectSettings.loadingMessage}
-            styles={selectStyles}
-          />
-
-          <label
-            className={
-              data?.applicantTypeIsRetired
-                ? "inputBox__form--readOnly-label"
-                : "inputBox__form--readOnly-label-hidden"
-            }
-          >
-            نوع بازنشسته
-          </label>
-        </div>
-
-        <div className="inputBox__form">
-          <Select
-            closeMenuOnSelect={true}
-            options={organizationOptions}
-            components={animatedComponents}
-            onChange={handleSelectOptionChange}
-            value={organizationOptions.find(
-              (item) => item.value === data?.organizationID
-            )}
-            name="organizationID"
-            isLoading={organizationIsLoading || organizationIsFetching}
-            isClearable={true}
-            placeholder={<div className="react-select-placeholder">سازمان</div>}
-            noOptionsMessage={selectSettings.noOptionsMessage}
-            loadingMessage={selectSettings.loadingMessage}
-            styles={selectStyles}
-          />
-
-          <label
-            className={
-              data?.organizationID
-                ? "inputBox__form--readOnly-label"
-                : "inputBox__form--readOnly-label-hidden"
-            }
-          >
-            سازمان
-          </label>
-        </div>
-
-        <div className="inputBox__form">
-          <InputDatePicker
-            onChange={handleFromDateChange}
-            value={selectedFromDate}
-            onOpenChange={handleFromCalenderOpenChange}
-            format={"jYYYY/jMM/jDD"}
-            suffixIcon={<CalenderIcon color="action" />}
-            open={isFromCalenderOpen}
-            style={datePickerStyles}
-            wrapperStyle={datePickerWrapperStyles}
-            pickerProps={{
-              ref: fromCalenderRef,
+    <>
+      {dashboardReportIsLoading || dashboardReportIsFetching ? (
+        <Modal title={"در حال ایجاد گزارش"}>
+          <p className="paragraph-primary" style={{ textAlign: "center" }}>
+            لطفا منتظر بمانید...
+          </p>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "2rem 10rem",
             }}
-          />
-          <div className="inputBox__form--readOnly-label">تاریخ از</div>
-        </div>
+          >
+            <CircularProgress color="primary" />
+          </Box>
+        </Modal>
+      ) : null}
+      <section className="flex-col u-margin-bottom-md">
+        <form method="POST" className="grid grid--col-5" noValidate>
+          <div className="inputBox__form">
+            <Select
+              closeMenuOnSelect={true}
+              options={retiredTypeOptions}
+              components={animatedComponents}
+              onChange={handleSelectOptionChange}
+              value={retiredTypeOptions.find(
+                (item) => item.value === data?.applicantTypeIsRetired
+              )}
+              name="applicantTypeIsRetired"
+              isClearable={true}
+              placeholder={
+                <div className="react-select-placeholder">نوع بازنشسته</div>
+              }
+              noOptionsMessage={selectSettings.noOptionsMessage}
+              loadingMessage={selectSettings.loadingMessage}
+              styles={selectStyles}
+            />
 
-        <div className="inputBox__form">
-          <InputDatePicker
-            onChange={handleTillDateChange}
-            value={selectedTillDate}
-            onOpenChange={handleTillCalenderOpenChange}
-            format={"jYYYY/jMM/jDD"}
-            suffixIcon={<CalenderIcon color="action" />}
-            open={isTillDateCalenderOpen}
-            style={datePickerStyles}
-            wrapperStyle={datePickerWrapperStyles}
-            pickerProps={{
-              ref: tillCalenderRef,
-            }}
-          />
-          <div className="inputBox__form--readOnly-label">تاریخ تا</div>
-        </div>
-      </form>
-
-      <div style={{ marginRight: "auto" }}>
-        <LoadingButton
-          dir="ltr"
-          endIcon={<GenerateIcon />}
-          onClick={getDashbaordReportHandler}
-          loading={dashboardReportIsLoading || dashboardReportIsFetching}
-          variant="contained"
-          color="success"
-          sx={{ fontFamily: "sahel" }}
-        >
-          <span>تولید گزارش</span>
-        </LoadingButton>
-      </div>
-      {showGrids && (
-        <div className="flex-col flex-center">
-          <div className="flex-row">
-            <DashboardSumGrid data={sumTableData} />
-            <DashboardHouseRightGrid data={houseRightTableData} />
-          </div>
-
-          <div className="flex-center">
-            <h5
-              className="title-secondary"
-              style={{ marginBottom: "0", marginTop: "20px" }}
+            <label
+              className={
+                data?.applicantTypeIsRetired
+                  ? "inputBox__form--readOnly-label"
+                  : "inputBox__form--readOnly-label-hidden"
+              }
             >
-              تعداد افراد تحت تکفل
-            </h5>
+              نوع بازنشسته
+            </label>
           </div>
 
-          <div className="flex-row">
-            <div className="flex-col">
-              <UnderWarantyAmountMenGrid data={uderWarantyMenTableData} />
-              <SupplementaryGrid data={supplementaryTableData} />
-            </div>
-            <div className="flex-col">
-              <UnderWarantyAmountWomenGrid data={underWarantyWomenTableData} />
-              <SacrificeCondtionsGrid data={ssacrificeTableData} />
-            </div>
-          </div>
+          <div className="inputBox__form">
+            <Select
+              closeMenuOnSelect={true}
+              options={organizationOptions}
+              components={animatedComponents}
+              onChange={handleSelectOptionChange}
+              value={organizationOptions.find(
+                (item) => item.value === data?.organizationID
+              )}
+              name="organizationID"
+              isLoading={organizationIsLoading || organizationIsFetching}
+              isClearable={true}
+              placeholder={
+                <div className="react-select-placeholder">سازمان</div>
+              }
+              noOptionsMessage={selectSettings.noOptionsMessage}
+              loadingMessage={selectSettings.loadingMessage}
+              styles={selectStyles}
+            />
 
-          <div className="flex-center">
-            <h5
-              className="title-secondary"
-              style={{ marginBottom: "0", marginTop: "20px" }}
+            <label
+              className={
+                data?.organizationID
+                  ? "inputBox__form--readOnly-label"
+                  : "inputBox__form--readOnly-label-hidden"
+              }
             >
-              حق عائله و اولاد افراد تحت تکفل
-            </h5>
+              سازمان
+            </label>
           </div>
 
-          <div className="flex-row">
-            <RelatedRightMenGrid data={relatedRightMenTableData} />
-            <RelatedRightWomenGrid data={relatedRightWomenTableData} />
+          <div className="inputBox__form">
+            <InputDatePicker
+              onChange={handleFromDateChange}
+              value={selectedFromDate}
+              onOpenChange={handleFromCalenderOpenChange}
+              format={"jYYYY/jMM/jDD"}
+              suffixIcon={<CalenderIcon color="action" />}
+              open={isFromCalenderOpen}
+              style={datePickerStyles}
+              wrapperStyle={datePickerWrapperStyles}
+              pickerProps={{
+                ref: fromCalenderRef,
+              }}
+            />
+            <div className="inputBox__form--readOnly-label">تاریخ از</div>
           </div>
 
+          <div className="inputBox__form">
+            <InputDatePicker
+              onChange={handleTillDateChange}
+              value={selectedTillDate}
+              onOpenChange={handleTillCalenderOpenChange}
+              format={"jYYYY/jMM/jDD"}
+              suffixIcon={<CalenderIcon color="action" />}
+              open={isTillDateCalenderOpen}
+              style={datePickerStyles}
+              wrapperStyle={datePickerWrapperStyles}
+              pickerProps={{
+                ref: tillCalenderRef,
+              }}
+            />
+            <div className="inputBox__form--readOnly-label">تاریخ تا</div>
+          </div>
+        </form>
+
+        <div style={{ marginRight: "auto" }}>
+          <LoadingButton
+            dir="ltr"
+            endIcon={<GenerateIcon />}
+            onClick={getDashbaordReportHandler}
+            loading={dashboardReportIsLoading || dashboardReportIsFetching}
+            variant="contained"
+            color="success"
+            sx={{ fontFamily: "sahel" }}
+          >
+            <span>تولید گزارش</span>
+          </LoadingButton>
+        </div>
+        {showGrids && (
           <div className="flex-col flex-center">
-            <SacrificeAmountGrid data={sacrificeAmountTableData} />
+            <div className="flex-row">
+              <DashboardSumGrid data={sumTableData} />
+              <DashboardHouseRightGrid data={houseRightTableData} />
+            </div>
+
+            <div className="flex-center">
+              <h5
+                className="title-secondary"
+                style={{ marginBottom: "0", marginTop: "20px" }}
+              >
+                تعداد افراد تحت تکفل
+              </h5>
+            </div>
+
+            <div className="flex-row">
+              <div className="flex-col">
+                <UnderWarantyAmountMenGrid data={uderWarantyMenTableData} />
+                <SupplementaryGrid data={supplementaryTableData} />
+              </div>
+              <div className="flex-col">
+                <UnderWarantyAmountWomenGrid
+                  data={underWarantyWomenTableData}
+                />
+                <SacrificeCondtionsGrid data={ssacrificeTableData} />
+              </div>
+            </div>
+
+            <div className="flex-center">
+              <h5
+                className="title-secondary"
+                style={{ marginBottom: "0", marginTop: "20px" }}
+              >
+                حق عائله و اولاد افراد تحت تکفل
+              </h5>
+            </div>
+
+            <div className="flex-row">
+              <RelatedRightMenGrid data={relatedRightMenTableData} />
+              <RelatedRightWomenGrid data={relatedRightWomenTableData} />
+            </div>
+
+            <div className="flex-col flex-center">
+              <SacrificeAmountGrid data={sacrificeAmountTableData} />
+            </div>
+            <MinSalaryGrid data={minSalaryTableData} />
           </div>
-          <MinSalaryGrid data={minSalaryTableData} />
-        </div>
-      )}
-    </section>
+        )}
+      </section>
+    </>
   );
   return content;
 }
